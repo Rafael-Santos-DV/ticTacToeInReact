14bbec44e391eccf2e37e710c3759f1538dc01e5
Rafael Santos <santosrafaell187@gmail.com>
2022-05-10 18:17:24 -0300

    add styles global

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function App() {
-    return <div className="App" />;
+  return (
+    <div className="App" style={{ border: '1px solid red', padding: '40px' }} />
+  );
 }
 
 export default App;
