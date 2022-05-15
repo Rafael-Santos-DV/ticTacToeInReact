888410a9b52298a6ad88c1ed4e5bb33f1c8b7a1e
Rafael Santos <santosrafaell187@gmail.com>
2022-05-15 10:32:05 -0300

    remove bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AuthContextProvider } from './context/ContextAuth';
 import { Game } from './pages/Game/Game';
